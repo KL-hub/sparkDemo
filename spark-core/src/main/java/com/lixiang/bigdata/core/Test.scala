20c575be20b8c933c0c6d88a9095291ae313b6a8
KL <1368781197@qq.com>
2021-02-12 21:53:03 +0800

    sacle =>wordcount

---
+++ spark-core/src/main/java/com/lixiang/bigdata/core/Test.scala
@@ -0,0 +1,7 @@
+package com.lixiang.bigdata.core
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    print("Hello spark")
+  }
+}
---
+++ spark-core/src/main/java/com/lixiang/bigdata/core/wc/Spark01_WordCount.scala
@@ -0,0 +1,31 @@
+package com.lixiang.bigdata.core.wc
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_WordCount {
+  def main(args: Array[String]): Unit = {
+    // appliation 调用  spark框架？？
+    //TODO 简历与spark的链接
+    val sparkConf =new SparkConf().setMaster("local").setAppName("WordCount")
+    val sc=new SparkContext(sparkConf)
+    //TODO 执行业务操作
+    //1、读取文件，获取一行一行的数据
+    val lines:RDD[String]=sc.textFile("datas/1.txt")
+    //2、将一行数据进行拆分，形成一个一个的单词
+    val words:RDD[String]=lines.flatMap(_.split(" "))
+    //3、将数据根据单词进行分组，便于统计
+    val wordGroup:RDD[(String,Iterable[String])] = words.groupBy(word => word)
+    //4、对分组后的数据进行转换
+    var wordToCount=wordGroup.map{
+      case (word,list)=>{
+        (word,list.size)
+      }
+    }
+    //5、将转换结果采集到控制台打印出来
+    val array:Array[(String,Int)]= wordToCount.collect()
+    array.foreach(println)
+    //TODO 关闭链接
+    sc.stop()
+  }
+}
