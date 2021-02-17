94c7413d3ba5d1bcaa64251f16a63905f2f24915
KL <1368781197@qq.com>
2021-02-17 19:24:11 +0800

    创建 RDD 的几种方式

---
+++ spark-core/src/main/java/com/lixiang/bigdata/core/rdd/builder/Spark02_RDD_File_Par.scala
@@ -0,0 +1,24 @@
+package com.lixiang.bigdata.core.rdd.builder
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * 从文件中创建Rdd ，并+分区
+ */
+object Spark02_RDD_File_Par {
+  def main(args: Array[String]): Unit = {
+    //TODO 准备环境
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+    val sc = new SparkContext(sparkConf)
+    //TODO 创建环境
+    //RDD的并行度 & 分区
+    //makeRDD 方法可以传递  第二个参数，这个参数表示分区的数量
+    //第二个参数可以不传递，那么makeRDD方法会使用默认值
+    //val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4), 2)
+    val rdd: RDD[String] = sc.textFile("datas/1.txt", 3)
+    rdd.saveAsTextFile("OUTPUT")
+    //TODO 关闭环境
+    sc.stop()
+  }
+}
